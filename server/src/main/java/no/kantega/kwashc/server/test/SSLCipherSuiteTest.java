6ee138a02ee5fe3a931f8f7abc2095b9a07d52df
Espen Fossen <espenaf@junta.no>
2015-04-09 21:48:54 +0200

    Added all know weak java 7/8 cipher suites.

--- server/src/main/java/no/kantega/kwashc/server/test/SSLCipherSuiteTest.java
+++ server/src/main/java/no/kantega/kwashc/server/test/SSLCipherSuiteTest.java
@@ -116,7 +116,23 @@ public class SSLCipherSuiteTest extends AbstractTest {
 
         try {
 
-            String[] ciphers = new String[]{"SSL_RSA_WITH_3DES_EDE_CBC_SHA", "SSL_DHE_RSA_WITH_DES_CBC_SHA", "SSL_DHE_DSS_WITH_DES_CBC_SHA", "SSL_RSA_WITH_NULL_MD5", "SSL_RSA_WITH_NULL_SHA", "SSL_DH_anon_EXPORT_WITH_DES40_CBC_SHA", "SSL_DH_anon_EXPORT_WITH_RC4_40_MD5", "TLS_KRB5_EXPORT_WITH_DES_CBC_40_MD5", "TLS_KRB5_EXPORT_WITH_DES_CBC_40_SHA", "TLS_DH_anon_WITH_AES_128_GCM_SHA256"};
+            String[] ciphers = new String[]{
+                    "SSL_RSA_WITH_3DES_EDE_CBC_SHA",
+                    "SSL_DHE_RSA_WITH_DES_CBC_SHA",
+                    "SSL_DHE_DSS_WITH_DES_CBC_SHA",
+                    "SSL_RSA_WITH_NULL_MD5",
+                    "SSL_RSA_WITH_NULL_SHA",
+                    "SSL_DH_anon_EXPORT_WITH_DES40_CBC_SHA",
+                    "SSL_DH_anon_EXPORT_WITH_RC4_40_MD5",
+                    "TLS_KRB5_EXPORT_WITH_DES_CBC_40_MD5",
+                    "TLS_KRB5_EXPORT_WITH_DES_CBC_40_SHA",
+                    "TLS_DH_anon_WITH_AES_128_GCM_SHA256",
+                    "TLS_ECDHE_ECDSA_WITH_RC4_128_SHA",
+                    "TLS_ECDHE_RSA_WITH_RC4_128_SHA",
+                    "TLS_RSA_WITH_RC4_128_SHA",
+                    "TLS_ECDH_ECDSA_WITH_RC4_128_SHA",
+                    "TLS_ECDH_RSA_WITH_RC4_128_SHA",
+                    "TLS_RSA_WITH_RC4_128_MD5"};
 
             HttpResponse response = checkClientForCiphers(site, httpsPort, httpclient, ciphers);
 
